30f94511e8b686f4dd91a5d8f38c5a6c1e1bc777
Alex <alejandro10montes@gmail.com>
2024-12-08 20:25:25 +0100

    Add secrets

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,6 +35,11 @@ android {
     }
 }
 
+secrets {
+    propertiesFileName = "secrets.properties"
+    defaultPropertiesFileName = "local.defaults.properties"
+}
+
 dependencies {
 
     implementation(libs.appcompat)
